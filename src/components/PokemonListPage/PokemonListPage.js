02bd51f73e66e15efaa25f361b593a228dadbcc1
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2023-06-22 15:09:37 -0300

    pokedex-app

--- src/components/PokemonListPage/PokemonListPage.js
+++ src/components/PokemonListPage/PokemonListPage.js
@@ -4,25 +4,51 @@ import {Container,PokemonNumber,PokemonName,PokemonType,TypesContainer,Pokeball,
 import PokemonCard from "../PokemonCard/PokemonCard"
 import { api } from "../api"
 import  { globalContext } from "../contexts/GlobalContexts"
-function PokemonListPage() {
+import { Button, Flex} from "@chakra-ui/react"
+import pokebolagif from "../../Assets/pokebolagif.gif"
 
+function PokemonListPage() {
+  const [loading, setLoading] = useState(true)
   const {pokedex} = useContext(globalContext)
-const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]) 
+  const [page, setPage] = useState(0)
+  const [anterior, setAnterior]= useState(true)
+  const [proximo, setProximo]= useState(true)
 
 useEffect(() => {
-    api.get("/pokemon").then((res) => {
+    api.get("/pokemon", {
+      params: {
+        limit: 30,
+        offset: page*30
+      },
+    }).then((res) => {
+      setAnterior(res.data.previous)
+      setProximo(res.data.next)
       const results = res.data.results;
       console.log(results);
       const promise = results.map((result) => api.get(result.url));
       Promise.all(promise).then((responses) => {
         const pokemonData = responses.map((res) => res.data);
         console.log(pokemonData);
-        setPokemons(pokemonData)
+        
+        setTimeout(()=> {
+          setPokemons(pokemonData)
+          setLoading(false)
+        }, 2000)
+        
       });
     });
-  }, []);
+  }, [page]);
 
  
+  if (loading) {
+    return (
+      <>
+        <img src={pokebolagif} alt="pokebola" />
+      </>
+    );
+  }
+
     return <>
    <Container>
       
@@ -40,7 +66,12 @@ useEffect(() => {
        
         ))}
       
-    </Container></>
+    </Container>
+    <Flex>
+    <Button hidden={!anterior}  onClick={()=>setPage(page-1)}>Anterior</Button>
+        <Button disabled={!proximo} onClick={()=>setPage(page+1)}>Proximo</Button>
+    </Flex>
+    </>
 }
 
 export default PokemonListPage
